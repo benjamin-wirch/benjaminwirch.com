53f0d6707d1c6ab6a2ef6bad42f565f526ec797e
Benjamin Wirch <55960782+benjamin-wirch@users.noreply.github.com>
2024-06-20 11:19:24 -0500

    Updated to Vite

---
+++ src/components/BenjaminWirch.jsx
@@ -0,0 +1,10 @@
+import { useState, useEffect } from "react";
+
+export default function BenjaminWirch(props) {
+    return <div>
+        <h1>Benjamin Wirch's Website</h1>
+        <div style={{ maxWidth: "40rem", margin: "auto", textWrap: "pretty" }}>
+            <p>Example text. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas fringilla phasellus faucibus scelerisque eleifend donec. Ullamcorper velit sed ullamcorper morbi tincidunt. Aliquet nibh praesent tristique magna. Posuere morbi leo urna molestie at elementum eu facilisis sed. Imperdiet dui accumsan sit amet nulla facilisi morbi. Vitae proin sagittis nisl rhoncus mattis rhoncus urna neque. Metus aliquam eleifend mi in nulla posuere sollicitudin. Posuere morbi leo urna molestie at elementum eu. Vitae elementum curabitur vitae nunc. Vulputate odio ut enim blandit volutpat. Tellus molestie nunc non blandit. Arcu non sodales neque sodales ut etiam. Vestibulum morbi blandit cursus risus at ultrices mi. Eu turpis egestas pretium aenean pharetra magna ac.</p>
+        </div>
+    </div>
+}
---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import BadgerMart from './components/BenjaminWirch'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './index.css'
+import BenjaminWirch from './components/BenjaminWirch'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BenjaminWirch />
+)
